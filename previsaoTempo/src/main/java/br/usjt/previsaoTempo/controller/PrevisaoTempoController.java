4dd388f0953e17c0c3c54c82ea22cc3250a6b7f2
BlackChann <61983459+BlackChann@users.noreply.github.com>
2020-04-03 23:09:54 -0300

    Aula02

--- previsaoTempo/src/main/java/br/usjt/previsaoTempo/controller/PrevisaoTempoController.java
+++ previsaoTempo/src/main/java/br/usjt/previsaoTempo/controller/PrevisaoTempoController.java
@@ -9,36 +9,53 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.usjt.previsaoTempo.model.Tempo;
-import br.usjt.previsaoTempo.repository.TempoRepository; 
- 
+import br.usjt.previsaoTempo.repository.TempoRepository;
+import br.usjt.previsaoTempoService.TempoService; 
+
 @Controller 
 
 public class PrevisaoTempoController {  
-	@Autowired  
-	private TempoRepository tempoRepo;
-	
-	
+	//a injeção de dependência ocorre aqui  
+	//@Autowired  
+	//private TempoRepository tempoRepo;
+
+	@Autowired 
+	private TempoRepository tempoRepo;;
+
+
+	@Autowired
+	public PrevisaoTempoController (TempoRepository tempoRepo) {
+		this.tempoRepo = tempoRepo;
+	}
+
+
+	@Autowired
+	public void setAlunosRepo(TempoRepository tempoRepo) {
+		this.tempoRepo = tempoRepo;
+	}
+
 	@GetMapping ("/tempo") 
 	public ModelAndView listarTempo () {  
-		 
-		ModelAndView mv = new ModelAndView ("lista_tempo"); 
-		 
-		List <Tempo> tempos = tempoRepo.findAll(); 
-		
-		mv.addObject("tempos", tempos);     
-		
-		mv.addObject(new Tempo()); 
-		
-		return mv; 
-		
-	} 
-	
-	
-	@PostMapping("/alunos") 
-	public String salvar (Tempo tempo) {  
-		tempoRepo.save(tempo);  
-		return "redirect:/tempo"; 
-	} 
-		 
+
+		ModelAndView mv = new ModelAndView("lista_alunos");
+		// para modelar o formulário
+		mv.addObject(new Tempo());
+		// Busque a coleção com o service
+		List<Tempo> tempos = TempoService.listarTodos();
+		// adicione a coleção ao objeto ModelAndView
+		mv.addObject("tempos", tempos);
+		// devolva o ModelAndView
+		return mv;
+
+	}
+
+	@PostMapping("/tempo")
+	public String salvar(Tempo tempo) {
+		//salvando com o service
+		TempoService.salvar(tempo);
+		return "redirect:/tempo";
+	}
+
+
+
 }
-	 
